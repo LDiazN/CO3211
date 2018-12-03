9d0d3612ed799e8207475f98d342b5cb702de689
LDiazN <15-10420@usb.ve>
2018-12-03 17:36:18 -0400

    terminado lab7

--- Lab7/Ejercicio2.m
+++ Lab7/Ejercicio2.m
@@ -15,6 +15,29 @@ P = @(x) polyval(P1,x) + log(x);
 F = @(x) exp(x)./x;
 
 Y_poli = P(1.15)
-
 Y_oct = integral(F,1,1.15)
 
+%Ahora vamos a graficar ambas funciones en [1,4]
+x = 1:0.1:4;
+Y1_pol = 1:1:length(x);
+Y1_oct = 1:1:length(x);
+for i = 1:1:length(Y1_pol)
+    Y1_pol(i) = P(x(i));
+    Y1_oct(i) = integral(F,1,x(i));
+endfor
+
+figure
+
+plot(Y1_oct,x)
+
+hold on
+
+plot(Y1_pol,x)
+legend("octave","polinomio")
+legend("Location",'southeast')
+title("aproximación vs función de octave")
+
+%{
+Como vemos, las aproximaciones en este intervalo son bastante malas, y las fun-
+ciones solo se parecen en un intervalo pequeño y cercano al 0.
+%}
